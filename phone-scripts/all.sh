81e3c7e1386fb923870fb96ec6c43e185df9b5d1
Tor (torbuntu) <sorensentor@tuta.io>
2021-04-16 20:36:31 -0500

    Update user settings
    Creating user pine's xdg directories. Disable automatic brightness. Activate scale-to-fit so applications attempt to "fit" to the screen. This is mostly hacky.

--- phone-scripts/all.sh
+++ phone-scripts/all.sh
@@ -4,6 +4,15 @@ set -e
 bash /root/01-create-sudo-user.sh
 bash /root/02-install-packages.sh
 
+echo "Setting user Pine's XDG user directories..."
+sudo -u pine xdg-user-dirs-update
+
+echo "Disable automatic brightness..."
+gsettings set org.gnome.settings-daemon.plugins.power ambient-enabled false
+
+echo "Set Phoc's scale-to-fit active globally..."
+gsettings set sm.puri.phoc scale-to-fit true
+
 echo "Remove temp-resolv.conf"
 rm /etc/tmp-resolv.conf
 rm /etc/resolv.conf
