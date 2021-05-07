f408cb37787a66252e0e13ad4acdbcf5c6b3ad7a
Chris I <chris@chrisirwin.ca>
2021-05-07 02:09:20 -0400

    Allocate fedora.img as a sparse 6GB file (#111)

--- 00-create-empty-image.sh
+++ 00-create-empty-image.sh
@@ -7,12 +7,20 @@ echo "========================"
 echo "00-create-empty-image.sh"
 echo "========================"
 
-# Approximately 3GB image.
-echo "Create image called $OUT_NAME? Make sure this doesn't exist, or it will be zeroed out."
+# Approximately 6GB image.
+echo "Create image called $OUT_NAME? Make sure this doesn't exist, or it will be replaced."
 read -p "Continue? [y/N] " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-    echo "Creating blank 3GB file called $OUT_NAME."
-    dd if=/dev/zero of=$OUT_NAME iflag=fullblock bs=1M count=4400 && sync
+    if [[ -e "$OUT_NAME" ]]
+    then
+        echo "Removing pre-existing $OUT_NAME."
+        unlink $OUT_NAME
+    fi
+
+    # Create this as a sparse file
+    echo "Creating sparse blank 6GB file called $OUT_NAME."
+    dd if=/dev/zero of=$OUT_NAME bs=1 count=0 seek=6G && sync
+
 fi
